a7291bf7784907ad6ff813ca061d5089e2abd5cc
guilherme <guilhermedmlopes@hotmail.com>
2022-12-27 16:58:53 -0300

    Adding files with ordenation examples

---
+++ Algoritmos_ordenacao/index.js
@@ -0,0 +1,29 @@
+//Algoritmo de ordenação
+// Devemos pegar o livro mais barato e sua posição
+/* 
+Entendendo o conceito de algoritmos de ordenação
+const precosLivros = [25, 15, 30, 50, 45, 20];
+
+let maisBarato = 0;
+
+for (let atual = 0; atual < precosLivros.length; atual++) {
+    if (precosLivros[atual] < precosLivros[maisBarato]) {
+        maisBarato = atual
+    }
+    console.log(atual)
+    console.log(maisBarato)
+}
+
+console.log(`o livro mais barato custa ${precosLivros[maisBarato]}`)
+*/
+
+const livros = require("./listaLivros.js") // importando de listaLivros
+
+let maisBarato = 0;
+for (let atual = 0; atual < livros.length; atual++) {
+    if (livros[atual].preco < livros[maisBarato].preco) {
+        maisBarato = atual
+    }   
+}
+
+console.log(`o livro mais barato custa ${livros[maisBarato].preco} e o titulo é ${livros[maisBarato].titulo}`)
---
+++ Algoritmos_ordenacao/listaLivros.js
@@ -0,0 +1,31 @@
+// Listar os livros em ordem de preços
+
+const precosLivros = [
+    {
+        titulo: "JavaScript",
+        preco: 25
+    }, 
+    {
+        titulo: "PHP",
+        preco: 15
+    }, 
+    {
+        titulo: "Java",
+        preco: 30
+    }, 
+    {
+        titulo: "Elixir",
+        preco: 50
+    }, 
+    {
+        titulo: "Go",
+        preco: 45
+    }, 
+    {
+        titulo: "Python",
+        preco: 20
+    }
+];
+
+//Exporta o valor para qualquer arquivo que for utilizar o listaLivros
+module.exports = precosLivros;
